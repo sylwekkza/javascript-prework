ca80d0889fa146979c90116daea0dbbbb63b424b
Sylwester Zajac <sylwekkza@gmail.com>
2021-04-09 12:23:28 +0200

    update game logic

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
 	computerMove = 'kamień';
-} if(randomNumber == 2){
+} else if(randomNumber == 2){
 	computerMove = 'papier';
-} if(randomNumber == 3){
+} else if(randomNumber == 3){
 	computerMove = 'nożyce';
 }
 
@@ -22,12 +22,63 @@ let playerMove = 'nieznany ruch';
 
 if(playerInput == 1){
 	playerMove = 'kamień';
-}
-if(playerInput == 2){
+} else if (playerInput == 2){
 	playerMove = 'papier';
-}
-if(playerInput == 3){
+} else if (playerInput == 3){
 	playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+	printMessage('Wygrałeś!');
+} else if(computerMove == 'kamień' && playerMove =='nożyce'){
+	printMessage('Przegrałeś');
+} else if(computerMove == 'kamień' && playerMove =='kamień'){
+	printMessage('Remis!');
+} else if(computerMove == 'papier' && playerMove == 'nożyce'){
+	printMessage('Wygrałeś!');
+} else if(computerMove == 'papier' && playerMove =='kamień'){
+	printMessage('Przegrałeś');
+} else if(computerMove == 'papier' && playerMove =='papier'){
+	printMessage('Remis!');
+} else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+	printMessage('Wygrałeś!');
+} else if(computerMove == 'nożyce' && playerMove =='papier'){
+	printMessage('Przegrałeś');
+} else if(computerMove == 'nożyce' && playerMove =='nożyce'){
+	printMessage('Remis!');
+} else {
+	printMessage('Błąd! Wybierz liczbę od 1 do 3 odpowiadającą zagraniom!');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
