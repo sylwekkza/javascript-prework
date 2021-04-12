c8a5eb2014c315915653f0f44809d6dea2c95a36
Sylwester Zajac <sylwekkza@gmail.com>
2021-04-12 20:54:47 +0200

    Change equal operator and tide up code

--- js/functions.js
+++ js/functions.js
@@ -2,49 +2,55 @@ let rock = 'kamień';
 let paper = 'papier';
 let scissors = 'nożyce';
 
-function printMessage(msg){
+function printMessage(message) {
 	let div = document.createElement('div');
-	div.innerHTML = msg;
+	div.innerHTML = message;
 	document.getElementById('messages').appendChild(div);
 }
 
-function clearMessages(){
+function clearMessages() {
 	document.getElementById('messages').innerHTML = '';
 }
 
-function getMoveName(argMoveId){
-	if (argMoveId == 1)
+function getMoveName(argMoveId) {
+	if (argMoveId === 1 || argMoveId === '1')
 		return rock;
 
-	if (argMoveId == 2)
+	if (argMoveId === 2 || argMoveId === '2')
 		return paper;
 
-	if (argMoveId == 3)
+	if (argMoveId === 3 || argMoveId === '3')
 		return scissors;
 
 		return 'nieznany ruch';
 }
 
-function displayResult(computerMove, playerMove){
+function displayResult(computerMove, playerMove) {
 	printMessage('Zagrałem ' + computerMove + ' , a Ty ' + playerMove);
 
-	if (computerMove == rock && playerMove == paper ||
-		computerMove == paper && playerMove == scissors ||
-		computerMove == scissors && playerMove == rock){
-			printMessage('Wygrywasz!');
+	if 
+	(computerMove === rock && playerMove === paper ||
+	computerMove === paper && playerMove === scissors ||
+	computerMove === scissors && playerMove === rock) {
+		printMessage ('Wygrywasz!');
+	} 
+	
+	else if 
+	(computerMove === rock && playerMove === scissors ||
+	computerMove === paper && playerMove === rock || 
+	computerMove === scissors && playerMove === paper) {
+		printMessage('Przegrałeś!');
+	} 
 
-	} else if (computerMove == rock && playerMove == scissors ||
-			   computerMove == paper && playerMove == rock || 
-			   computerMove == scissors && playerMove == paper){
-					printMessage('Przegrałeś!');
+	else if 
+	(computerMove === rock && playerMove === rock ||
+	computerMove === paper && playerMove === paper ||
+	computerMove === scissors && playerMove === scissors) {
+		printMessage ('Remis!');
+	} 
 
-	} else if (computerMove == rock && playerMove == rock ||
-			   computerMove == paper && playerMove == paper ||
-	           computerMove == scissors && playerMove == scissors){
-				   	printMessage('Remis!');
-
-	} else {
-		printMessage('Błąd! Wybierz liczbę od 1 do 3 odpowiadającą zagraniom!');
+	else {
+		printMessage ('Błąd! Wybierz liczbę od 1 do 3 odpowiadającą zagraniom!');
 	}
 		
 }
