a9e28fad731ae814fc3171ff24954ffd6f6e95c3
Sylwester Zajac <sylwekkza@gmail.com>
2021-04-13 10:39:25 +0200

    add buttons for moves, delete prompt pop up upon site load

--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,21 @@ let rock = 'kamień';
 let paper = 'papier';
 let scissors = 'nożyce';
 
+function playGame(playerInput){
+
+	clearMessages
+
+	let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+	let computerMove = getMoveName(randomNumber);
+
+	//let playerInput = prompt('Wybierz swój ruch! 1: kamień 2: papier 3: nożyce');
+
+	let playerMove = getMoveName(playerInput);
+
+	let result = displayResult(computerMove, playerMove);
+}
+
 function printMessage(message) {
 	let div = document.createElement('div');
 	div.innerHTML = message;
@@ -14,7 +29,7 @@ function clearMessages() {
 
 function getMoveName(argMoveId) {
 	
-	let moveId = Number(argMoveId);
+	let moveId = parseInt(argMoveId);
 
 	if (moveId === 1)
 		return rock;
--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-let randomNumber = Math.floor(Math.random() * 3 + 1);
+document.getElementById('play-rock').addEventListener('click', function(){
+	playGame(1);
+});
 
-let computerMove = getMoveName(randomNumber);
+document.getElementById('play-paper').addEventListener('click', function(){
+	playGame(2);
+});
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień 2: papier 3: nożyce');
-
-let playerMove = getMoveName(playerInput);
-
-let result = displayResult(computerMove, playerMove);
+document.getElementById('play-scissors').addEventListener('click', function(){
+	playGame(3);
+});
