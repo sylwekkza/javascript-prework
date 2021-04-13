56285258fc9a15770b96f896971139c11e8b0a6c
Sylwester Zajac <sylwekkza@gmail.com>
2021-04-13 10:41:23 +0200

    add clearMessages function

--- js/functions.js
+++ js/functions.js
@@ -2,20 +2,6 @@ let rock = 'kamień';
 let paper = 'papier';
 let scissors = 'nożyce';
 
-function playGame(playerInput){
-
-	clearMessages
-
-	let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-	let computerMove = getMoveName(randomNumber);
-
-	//let playerInput = prompt('Wybierz swój ruch! 1: kamień 2: papier 3: nożyce');
-
-	let playerMove = getMoveName(playerInput);
-
-	let result = displayResult(computerMove, playerMove);
-}
 
 function printMessage(message) {
 	let div = document.createElement('div');
--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-document.getElementById('play-rock').addEventListener('click', function(){
+function playGame(playerInput){
+
+	clearMessages()
+
+	let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+	let computerMove = getMoveName(randomNumber);
+
+	//let playerInput = prompt('Wybierz swój ruch! 1: kamień 2: papier 3: nożyce');
+
+	let playerMove = getMoveName(playerInput);
+
+	let result = displayResult(computerMove, playerMove);
+
+
+}document.getElementById('play-rock').addEventListener('click', function(){
 	playGame(1);
 });
 
