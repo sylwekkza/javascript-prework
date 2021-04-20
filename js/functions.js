c38e53777481a43d62c89b3a1ba99c939d1dbf19
Sylwester Zajac <sylwekkza@gmail.com>
2021-04-20 12:17:25 +0200

    put function declarations with their calls in order

--- js/functions.js
+++ js/functions.js
@@ -5,7 +5,7 @@
 			paper = 'papier',
 			scissors = 'nożyce';
 
-		const getMoveName = function (argMoveId) {
+		const getMoveName = function(argMoveId) {
 			const moveId = parseInt(argMoveId);
 
 			if (moveId === 1)
@@ -20,6 +20,10 @@
 			return 'nieznany ruch';
 		}
 
+		const randomNumber = Math.floor(Math.random() * 3 + 1),
+			computerMove = getMoveName(randomNumber),
+			playerMove = getMoveName(playerInput);
+
 		const displayResult = function(computerMove, playerMove) {
 			const printMessage = function(message) {
 				const div = document.createElement('div');
@@ -27,40 +31,36 @@
 				document.getElementById('messages').appendChild(div);
 			}
 			printMessage('Zagrałeś ' + playerMove + ', a Komputer wylosował ' + computerMove + '!');
-
+	
 			if (computerMove === rock && playerMove === paper ||
 			computerMove === paper && playerMove === scissors ||
 			computerMove === scissors && playerMove === rock) {
 			printMessage('<p class="result result-win">Wygrywasz!</p>');
 			} 
-		
+			
 			else if (computerMove === rock && playerMove === scissors ||
 			computerMove === paper && playerMove === rock || 
 			computerMove === scissors && playerMove === paper) {
 				printMessage('<p class="result result-lose">Przegrałeś!</p>');
 			} 
-
+	
 			else if (computerMove === rock && playerMove === rock ||
 			computerMove === paper && playerMove === paper ||
 			computerMove === scissors && playerMove === scissors) {
 				printMessage('<p class="result result-draw">Remis!</p>');
 			} 
-
+	
 			else {
 				printMessage('Błąd! Wybierz liczbę od 1 do 3 odpowiadającą zagraniom!');
 			}	
 		}
-
+			
 		const clearMessages = function() {
 			document.getElementById('messages').innerHTML = '';
 		}
-
+	
 		clearMessages();
-
-		const randomNumber = Math.floor(Math.random() * 3 + 1),
-			computerMove = getMoveName(randomNumber),
-			playerMove = getMoveName(playerInput);
-
+		
 		displayResult(computerMove, playerMove);
 	}
 }
