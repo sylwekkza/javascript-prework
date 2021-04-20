f7412fc32515cd2f1f770dfdef2ff82898bd3a5c
Sylwester Zajac <sylwekkza@gmail.com>
2021-04-20 10:11:34 +0200

    change range of variables by closing all code in braces

--- js/functions.js
+++ js/functions.js
@@ -1,75 +1,71 @@
-let rock = 'kamień';
-let paper = 'papier';
-let scissors = 'nożyce';
-
-
-function printMessage(message) {
-	let div = document.createElement('div');
-	div.innerHTML = message;
-	document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages() {
-	document.getElementById('messages').innerHTML = '';
-}
-
-function getMoveName(argMoveId) {
-	
-	let moveId = parseInt(argMoveId);
-
-	if (moveId === 1)
-		return rock;
-
-	if (moveId === 2)
-		return paper;
-
-	if (moveId === 3)
-		return scissors;
-
-	return 'nieznany ruch';
-}
-
-function displayResult(computerMove, playerMove) {
-	printMessage('Zagrałeś ' + playerMove + ', a Komputer wylosował ' + computerMove + '!');
-
-	if 
-	(computerMove === rock && playerMove === paper ||
-	computerMove === paper && playerMove === scissors ||
-	computerMove === scissors && playerMove === rock) {
-		printMessage('Wygrywasz!');
-	} 
-	
-	else if 
-	(computerMove === rock && playerMove === scissors ||
-	computerMove === paper && playerMove === rock || 
-	computerMove === scissors && playerMove === paper) {
-		printMessage('Przegrałeś!');
-	} 
-
-	else if 
-	(computerMove === rock && playerMove === rock ||
-	computerMove === paper && playerMove === paper ||
-	computerMove === scissors && playerMove === scissors) {
-		printMessage('Remis!');
-	} 
-
-	else {
-		printMessage('Błąd! Wybierz liczbę od 1 do 3 odpowiadającą zagraniom!');
+{ 
+	function printMessage(message) {
+		let div = document.createElement('div');
+		div.innerHTML = message;
+		document.getElementById('messages').appendChild(div);
 	}
-		
+
+	function clearMessages() {
+		document.getElementById('messages').innerHTML = '';
+	}
+
+	{
+		let rock = 'kamień',
+			paper = 'papier',
+			scissors = 'nożyce';
+
+		function getMoveName(argMoveId) {
+
+			let moveId = parseInt(argMoveId);
+
+			if (moveId === 1)
+				return rock;
+
+			if (moveId === 2)
+				return paper;
+
+			if (moveId === 3)
+				return scissors;
+
+			return 'nieznany ruch';
+		}
+
+		function displayResult(computerMove, playerMove) {
+			printMessage('Zagrałeś ' + playerMove + ', a Komputer wylosował ' + computerMove + '!');
+
+			if (computerMove === rock && playerMove === paper ||
+			computerMove === paper && playerMove === scissors ||
+			computerMove === scissors && playerMove === rock) {
+			printMessage('<p class="result result-win">Wygrywasz!</p>');
+			} 
+	
+			else if (computerMove === rock && playerMove === scissors ||
+			computerMove === paper && playerMove === rock || 
+			computerMove === scissors && playerMove === paper) {
+				printMessage('<p class="result result-lose">Przegrałeś!</p>');
+			} 
+
+			else if (computerMove === rock && playerMove === rock ||
+			computerMove === paper && playerMove === paper ||
+			computerMove === scissors && playerMove === scissors) {
+				printMessage('<p class="result result-draw">Remis!</p>');
+			} 
+
+			else {
+				printMessage('Błąd! Wybierz liczbę od 1 do 3 odpowiadającą zagraniom!');
+			}	
+		}
+	}
+
+	function playGame(playerInput){
+		clearMessages();
+
+		let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+		let computerMove = getMoveName(randomNumber);
+
+		let playerMove = getMoveName(playerInput);
+
+		displayResult(computerMove, playerMove);
+	} 
 }
-
-function playGame(playerInput){
-
-	clearMessages()
-
-	let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-	let computerMove = getMoveName(randomNumber);
-
-	let playerMove = getMoveName(playerInput);
-
-	let result = displayResult(computerMove, playerMove);
-} 
-
-
