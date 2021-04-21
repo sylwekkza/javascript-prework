f76eb68072b4af7625bb8b6882d0cdec9b8d5366
Sylwester Zajac <sylwekkza@gmail.com>
2021-04-21 16:38:30 +0200

    add scoring for game

--- js/functions.js
+++ js/functions.js
@@ -4,6 +4,7 @@
 	const paper = 'papier';
 	const scissors = 'nożyce';
 	const messagesDiv = document.getElementById('messages');
+	const scoreDiv = document.getElementById('scores');
 
 	const getMoveName = function(argMoveId) {
 		const moveId = parseInt(argMoveId);
@@ -26,25 +27,43 @@
 		messagesDiv.appendChild(div);
 	}
 
+	const printMessage2 = function(message) {
+		const div = document.createElement('div');
+		div.innerHTML = message;
+		scoreDiv.appendChild(div);
+	}
+
+	let player = 0;
+	let computer = 0;
+	
 	const displayResult = function(computerMove, playerMove) {
 		printMessage('Zagrałeś ' + playerMove + ', a Komputer wylosował ' + computerMove + '!');
+
+		if(computer === 10 || player === 10){
+			location.reload();
+		}
 
 		if (computerMove === rock && playerMove === paper ||
 		computerMove === paper && playerMove === scissors ||
 		computerMove === scissors && playerMove === rock) {
 			printMessage('<p class="result result-win">Wygrywasz!</p>');
+			player++;
+			printMessage2(player + ':' + computer);
 		} 
 		
 		else if (computerMove === rock && playerMove === scissors ||
 		computerMove === paper && playerMove === rock || 
 		computerMove === scissors && playerMove === paper) {
 			printMessage('<p class="result result-lose">Przegrałeś!</p>');
+			computer++;
+			printMessage2(player + ':' + computer);
 		} 
 
 		else if (computerMove === rock && playerMove === rock ||
 		computerMove === paper && playerMove === paper ||
 		computerMove === scissors && playerMove === scissors) {
 			printMessage('<p class="result result-draw">Remis!</p>');
+			printMessage2(player + ':' + computer);
 		} 
 
 		else {
@@ -55,6 +74,10 @@
 	const clearMessages = function() {
 		messagesDiv.innerHTML = '';
 	}
+		
+	const clearMessages2 = function() {
+		scoreDiv.innerHTML = '';
+	}
 	
 	var playGame = function(playerInput) {
 		const randomNumber = Math.floor(Math.random() * 3 + 1);
@@ -64,7 +87,13 @@
 		const playerMove = getMoveName(playerInput);
 		
 		clearMessages();
+
+		clearMessages2();
 		
 		displayResult(computerMove, playerMove);
-	}		
+	}	
+	
+	
 }
+
+
